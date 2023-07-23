d58dbbd4fe5aa776bd8fd9e613740c9a43316c35
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-07-23 15:20:11 +0300

    Commit

---
+++ RedMangoAPI/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedMangoAPI.Data;
+using RedMangoAPI.Models;
+
+namespace RedMangoAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private ApiResponse _response;
+        private string secretKey;
+
+        public AuthController(ApplicationDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _response = new ApiResponse();
+        }
+    }
+}
---
+++ RedMangoAPI/Utils/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace RedMangoAPI.Utils
+{
+    public static class SD
+    {
+        public const string Role_Admin = "admin";
+        public const string Role_Customer = "customer";
+
+    }
+}

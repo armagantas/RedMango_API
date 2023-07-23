584ed59b604f195152bd821b0bfed1c4e55d7865
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-07-24 00:11:52 +0300

    Commit

--- RedMangoAPI/Data/ApplicationDbContext.cs
+++ RedMangoAPI/Data/ApplicationDbContext.cs
@@ -13,7 +13,9 @@ namespace RedMangoAPI.Data
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<MenuItem> MenuItems { get; set; }
-
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+ 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
